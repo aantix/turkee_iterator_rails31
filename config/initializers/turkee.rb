90c504edf1e3cc85acbdb8bc0662b0965d9b9490
Mark Percival <m@mdp.im>
2011-12-04 23:11:03 -0800

    Remove old key from git repo

--- config/initializers/turkee.rb
+++ config/initializers/turkee.rb
@@ -1,7 +1,7 @@
 # Go to this page https://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
 # to retrieve your AWS/Mechanical Turk access keys.
-AWSACCESSKEYID      = 'AKIAIAJENS4SKBG3WGLA'
-AWSSECRETACCESSKEY  = '3k5TG23pOoGGnY0kAP3YSy46AUQge+zMVR3ZKgF6'
+AWSACCESSKEYID      = ENV['AWS_KEY']
+AWSSECRETACCESSKEY  = ENV['AWS_SECRET']
 
 RTurk::logger.level = Logger::DEBUG
 RTurk.setup(AWSACCESSKEYID, AWSSECRETACCESSKEY, :sandbox => true)
