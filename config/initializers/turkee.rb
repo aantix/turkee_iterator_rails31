a09045ee9cffb7e19e8fa7c026c6f2965f891560
Mark Percival <m@mdp.im>
2011-12-04 21:47:08 -0800

    Updating with dev credentials

--- config/initializers/turkee.rb
+++ config/initializers/turkee.rb
@@ -1,7 +1,7 @@
 # Go to this page https://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
 # to retrieve your AWS/Mechanical Turk access keys.
-AWSACCESSKEYID      = 'XXXXXXXX2'
-AWSSECRETACCESSKEY  = 'YYYYYYYYYYYY'
+AWSACCESSKEYID      = 'AKIAIAJENS4SKBG3WGLA'
+AWSSECRETACCESSKEY  = '3k5TG23pOoGGnY0kAP3YSy46AUQge+zMVR3ZKgF6'
 
 RTurk::logger.level = Logger::DEBUG
-RTurk.setup(AWSACCESSKEYID, AWSSECRETACCESSKEY, :sandbox => (Rails.env == 'production' ? false : true))
+RTurk.setup(AWSACCESSKEYID, AWSSECRETACCESSKEY, :sandbox => true)
