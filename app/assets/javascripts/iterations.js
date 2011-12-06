2a04e9bcbf2000dc6508a27799996410aa8bf4c8
Jim Jones <jjones@aantix.com>
2011-12-06 00:13:52 -0800

    Voting functionality has been restored.

--- app/assets/javascripts/iterations.js
+++ app/assets/javascripts/iterations.js
@@ -4,4 +4,11 @@ $(document).ready(
       var task_id = $("#results").data("id");
       $('#results').load("/iterations/" + task_id + "/results");
     }, 3000);
-  });
+
+});
+
+
+function set_text(control, value) {
+  document.getElementById(control).value = value;
+}
+
